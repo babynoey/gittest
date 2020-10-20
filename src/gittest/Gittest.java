0c073a0aeabe6d767c8ef7616476593f54308f97
Sim <simsimsim21@gmail.com>
2020-10-20 17:12:31 +0700

    new_feature

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -17,6 +17,7 @@ public class Gittest {
     public static void main(String[] args) {
         // TODO code application logic here
         sayHello();
+
     }
     
     public static void sayHello() {
